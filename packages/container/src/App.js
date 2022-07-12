45e52e878f60ec927c9ddb4effadbc65a78b596b
Leandro M <lemil77@gmail.com>
2022-07-12 14:25:47 -0300

    initial commit

---
+++ packages/container/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import MaketingApp from './components/MaketingApp';
+
+export default() => {
+ return <div>
+    <h1>Hello!</h1>
+    <hr/>
+    <MaketingApp />
+ </div>
+
+};
+
