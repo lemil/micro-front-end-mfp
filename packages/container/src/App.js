fe5e0578c533ec0c5efe4a672f7baa7bded1d205
lemil <lemil77@gmail.com>
2022-07-14 09:06:54 -0300

    classnames fixed

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -2,15 +2,23 @@ import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import MaketingApp from './components/MaketingApp';
 import Header from './components/Header'
+import { StylesProvider, createGenerateClassName } from '@material-ui/core/styles';
+
+
+const generatedClassName = createGenerateClassName({
+   'co',
+});
 
 export default() => {
  return (
-   <BrowserRouter>
-      <div>
-         <Header />
-         <MaketingApp />
-      </div>
-   </BrowserRouter>
+   <StylesProvider generateClassName={generatedClassName}>
+      <BrowserRouter>
+         <div>
+            <Header />
+            <MaketingApp />
+         </div>
+      </BrowserRouter>
+   </StylesProvider>
  );
 
 };
--- packages/marketing/src/App.js
+++ packages/marketing/src/App.js
@@ -2,11 +2,15 @@
 
 import React from 'react';
 import {Switch, Route, BrowserRouter} from 'react-router-dom';
-import { StylesProvider } from '@material-ui/core/styles';
+import { StylesProvider, createGenerateClassName } from '@material-ui/core/styles';
 
 
 import Landing from './components/Landing';
 import Pricing from './components/Pricing';
+
+const generatedClassName = createGenerateClassName({
+    'ma',
+});
 
 export default() => {
     return <div>
